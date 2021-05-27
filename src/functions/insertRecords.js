bb241451514cc0fccbd77a8f69800e1df94bc9e1
José Antonio González <jagonzalr@users.noreply.github.com>
2021-05-27 14:46:24 +0200

    Insert Records (#4)
    * Receive csv records from sqs queue an insert them on dynamodb
    
    * Update insert of records to use batch write

--- src/functions/insertRecords.js
+++ src/functions/insertRecords.js
@@ -1,4 +1,34 @@
+'use strict'
+
+import AWS from 'aws-sdk'
+
+const REGION = process.env.AWS_REGION
+const USERS_TABLE = process.env.USERS_TABLE
+
+const db = new AWS.DynamoDB.DocumentClient({
+  convertEmptyValues: true,
+  region: REGION
+})
+
 export const handler = async event => {
-  console.log(JSON.stringify(event, null, 2))
+  if (event && event.Records) {
+    const records = event.Records
+    const putRequests = records.map(({ body }) => {
+      return {
+        PutRequest: {
+          Item: JSON.parse(body)
+        }
+      }
+    })
+    
+    const params = {
+      RequestItems: {
+        [USERS_TABLE]: putRequests
+      }
+    }
+
+    await db.batchWrite(params).promise()
+  }
+
   return { statusCode: 200 }
 }
